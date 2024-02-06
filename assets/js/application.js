921f8217536d4a0acce54e2c0779e19047ea5b63
smoke-ck <2056sanya9snake@gmail.com>
2024-02-06 11:09:07 +0200

    Add basic structure, and posts

---
+++ assets/js/application.js
@@ -0,0 +1,30 @@
+if (document.readyState !== "loading") { initTransitionView() }
+document.addEventListener("DOMContentLoaded", initTransitionView);
+
+function initTransitionView() {
+  if (screen.width > 748) {
+    setActiveNavLink();
+    const transition_links = document.querySelectorAll('[data-link="transition"]')
+
+    transition_links.forEach((link) => {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const name = e.target.href.split('/').filter(word => word !== '').pop()
+        link.style.setProperty("view-transition-name", name)
+
+        setTimeout(function() { window.location.href = e.target.href; });
+      });
+    });
+  }
+}
+
+function setActiveNavLink() {
+  const navbar_links = document.querySelectorAll('[data-link="nav-link"]');
+  const currentUrl = window.location.href;
+  navbar_links.forEach(function(link) {
+
+    if (currentUrl.includes(link.href)) {
+      link.classList.add("active");
+    }
+  });
+}
