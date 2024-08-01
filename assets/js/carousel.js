c7f10fb873518b5143b19923d3a410f92aa274dd
Smoke-ck <2056sanya9snake@gmail.com>
2024-08-01 12:56:01 +0300

    Add more content to carousel cards, fix vertical page scroll

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,19 +1,112 @@
-// document.addEventListener("DOMContentLoaded", initCarousels );
-// if (document.readyState !== "loading") { initCarousels() }
-
 window.onload = initCarousels;
 
-
 function initCarousels() {
-  carousels = document.querySelectorAll('[data-scroll="scroller"]')
-  carousels.forEach((element) => {setCarousel(element)})
+  const carousels = document.querySelectorAll('[data-scroll="scroller"]');
+  carousels.forEach((element) => setCarousel(element));
 }
 
 function setCarousel(scroller) {
+  let items = scroller.querySelectorAll('.animate-visibility');
+  let clones = [];
+  let disableScroll = false;
+  let scrollWidth = 0;
+  let scrollPos = 0;
+  let clonesWidth = 0;
   let isScrolling = false;
+  scroller.innerHTML += scroller.innerHTML;
+  const getScrollPos = () => { return scroller.scrollLeft; };
+  const setScrollPos = (pos) => { scroller.scrollLeft = pos; };
 
-  scroller.innerHTML += scroller.innerHTML
-  scroller.scrollLeft = 1600;
+  items.forEach(item => {
+    const clone = item.cloneNode(true);
+    scroller.appendChild(clone);
+    clone.classList.add('js-clone');
+  });
+
+  clones = scroller.querySelectorAll('.js-clone');
+  getClonesWidth();
+  let mobileStep = screen.width <= 768 ? clones[0].offsetWidth/2 : 0;
+  setScrollPos(clonesWidth - mobileStep);
+  reCalc();
+
+  function getClonesWidth() {
+    clonesWidth = 0;
+    clones.forEach(clone => { clonesWidth += clone.offsetWidth; });
+
+    return clonesWidth;
+  }
+
+  function reCalc() {
+    scrollPos = getScrollPos();
+    scrollWidth = scroller.scrollWidth;
+    clonesWidth = getClonesWidth();
+    if (scrollPos <= 0) { setScrollPos(1); }
+  }
+
+  function scrollUpdate() {
+    if (!disableScroll) {
+      scrollPos = getScrollPos();
+      let val = screen.width <= 768 ? clones[0].offsetWidth/2 : 0
+      if (clonesWidth + scrollPos >= scrollWidth) {
+        setScrollPos(clonesWidth + val);
+        disableScroll = true;
+      } else if (scrollPos == 0 ) {
+        setScrollPos(scrollWidth - clonesWidth * 2);
+        disableScroll = true;
+      } else if(scrollPos <= val ){
+        setScrollPos(scrollWidth - clonesWidth * 2 + val);
+        disableScroll = true;
+      }
+    }
+    if (disableScroll) {
+      setTimeout(() => {
+        disableScroll = false;
+      }, 50);
+    }
+  }
+
+  function handleWheelEvent(e) {
+    e.preventDefault();
+    if (disableScroll) return;
+
+    if (Math.abs(e.deltaX) > Math.abs(e.deltaY)) {
+      const delta = Math.sign(e.deltaX) * scroller.children[0].offsetWidth;
+      smoothScroll(scroller, scroller.scrollLeft + delta);
+    } else {
+      window.scrollBy(0, e.deltaY);
+    }
+  }
+
+  function smoothScroll(element, to) {
+    if (isScrolling) return;
+    isScrolling = true;
+
+    const start = element.scrollLeft;
+    const change = to - start;
+    const duration = 400;
+    let currentTime = 0;
+
+    function animateScroll() {
+      currentTime += 20;
+      const val = easeInOutQuad(currentTime, start, change, duration);
+      element.scrollLeft = val;
+      if (currentTime < duration) {
+        requestAnimationFrame(animateScroll);
+      } else {
+        isScrolling = false;
+        requestAnimationFrame(scrollUpdate);
+      }
+    }
+
+    animateScroll();
+  }
+
+  function easeInOutQuad(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+  }
 
   scroller.addEventListener("click", (event) => {
     const rect = event.currentTarget.getBoundingClientRect();
@@ -27,119 +120,46 @@ function setCarousel(scroller) {
     }
   });
 
-  function handleWheelEvent(e) {
-    e.preventDefault();
-    const delta = Math.sign(e.deltaX) * scroller.children[0].offsetWidth;
-    smoothScroll(scroller, scroller.scrollLeft + delta);
-  }
-
   scroller.addEventListener('wheel', handleWheelEvent, { passive: false });
   scroller.addEventListener('touchstart', onTouchStart, { passive: false });
   scroller.addEventListener('touchmove', onTouchMove, { passive: false });
 
   let startX = 0;
+  let startY = 0;
   let isDragging = false;
 
   function onTouchStart(event) {
     startX = event.touches[0].pageX;
+    startY = event.touches[0].pageY;
     isDragging = true;
   }
+
   function onTouchMove(event) {
-    if (!isDragging) return;
-
     const currentX = event.touches[0].pageX;
-    const deltaX = currentX - startX > 0 ? -1 : 1;
-    smoothScroll(scroller, scroller.scrollLeft + (deltaX * scroller.children[0].offsetWidth ))
-  }
+    const currentY = event.touches[0].pageY;
+    const deltaX = currentX - startX;
+    const deltaY = currentY - startY;
 
-  function smoothScroll(element, to) {
-    if (isScrolling) return;
-    isScrolling = true;
-
-    const start = element.scrollLeft;
-    const change = to - start;
-    const duration = 300;
-    let currentTime = 0;
-
-    const { scrollLeft, scrollWidth, offsetWidth } = scroller
-    const progress = scrollLeft / (scrollWidth - offsetWidth) * 100
-
-
-    const isForward = (window.scrollProgress <= progress)
-    window.scrollProgress = progress;
-
-    // console.log(scrollLeft)
-    // console.log(scrollLeft <= offsetWidth && isForward )
-
-    if(offsetWidth + scrollLeft >= scrollWidth - offsetWidth && isForward) {
-      console.log('rigth')
-      scroller.scrollLeft = scrollLeft - scrollWidth / 2
-      window.scrollProgress = 0
-      isScrolling = false;
-    } else if (scrollLeft <= offsetWidth && !isForward) {
-      console.log('left')
-      const setPos = () => { scroller.scrollLeft = scrollLeft + scrollWidth / 2};
-      requestAnimationFrame(setPos);
-
-      window.scrollProgress = 100
-      isScrolling = false;
+    if (Math.abs(deltaX) > Math.abs(deltaY) && isDragging) {
+      const scrollDelta = currentX - startX > 0 ? -1 : 1;
+      smoothScroll(scroller, scroller.scrollLeft + (scrollDelta * scroller.children[0].offsetWidth ))
     } else {
-      animateScroll();
+      window.scrollBy(0, -deltaY);
+      isDragging = false;
     }
-
-    function animateScroll() {
-      currentTime += 15;
-      const val = easeInOutQuad(currentTime, start, change, duration);
-      element.scrollLeft = val;
-      if (currentTime < duration) {
-        isScrolling = true;
-
-        requestAnimationFrame(animateScroll);
-      } else {
-        isScrolling = false;
-      }
-    }
-
-
   }
-
-  function easeInOutQuad(t, b, c, d) {
-    t /= d / 2;
-    if (t < 1) return c / 2 * t * t + b;
-    t--;
-    return -c / 2 * (t * (t - 2) - 1) + b;
-  }
-
 }
 
-function loop({ target, target: { scrollLeft, scrollWidth, offsetWidth } }) {
-  const progress = scrollLeft / (scrollWidth - offsetWidth) * 100
-  if (window.scrollProgress === progress) return
-
-  const isForward = (window.scrollProgress <= progress)
-  window.scrollProgress = progress;
-
-  [...target.children].map(e => {
-    const toCenter = Math.abs(window.outerWidth / 2 - e.getBoundingClientRect().left - e.getBoundingClientRect().width / 2)
-    const toCenter2 = (window.outerWidth / 2 - e.getBoundingClientRect().left - e.getBoundingClientRect().width / 2)
-    const viewport = toCenter / offsetWidth * 100
-    const viewport2 = toCenter2 / offsetWidth * 100
-    e.style.setProperty('--viewport', viewport)
-    e.style.setProperty('--viewport2', viewport2)
-  })
-  target.style.setProperty('--scroll', Math.floor(progress))
-  // console.log('scrollLeft', scrollLeft)
-
-  // if (offsetWidth + scrollLeft >= scrollWidth - offsetWidth && isForward) {
-  //   console.log('rigth')
-  //   target.scrollLeft = scrollLeft - scrollWidth / 2
-  //   window.scrollProgress = 0
-  //   return
-  // }
-  // if (scrollLeft <= offsetWidth && !isForward) {
-  //   console.log('left')
-  //   target.scrollLeft = scrollLeft + scrollWidth / 2
-  //   window.scrollProgress = 100
-  //   return
-  // }
+function loop({ target, target: { offsetWidth } }) {
+  const checkPos = () => {
+    [...target.children].map(e => {
+      const toCenter = Math.abs(window.outerWidth / 2 - e.getBoundingClientRect().left - e.getBoundingClientRect().width / 2)
+      const toCenter2 = window.outerWidth / 2 - e.getBoundingClientRect().left - e.getBoundingClientRect().width / 2
+      const viewport = toCenter / offsetWidth * 100
+      const viewport2 = toCenter2 / offsetWidth * 100
+      e.style.setProperty('--viewport', viewport)
+      e.style.setProperty('--viewport2', viewport2)
+    })
+  }
+  requestAnimationFrame(checkPos)
 }
